ba4efd1712edff8756bf0cc7c12a1a27635d75b7
Peter Sirotnak <sirotnak.p@gmail.com>
2025-05-12 11:35:45 +0200

    PMM-7: List admin tasks

--- pmm-tests/agent_status.py
+++ pmm-tests/agent_status.py
@@ -1,3 +1,9 @@
+import argparse
+import os
+import subprocess
+import sys
+import unittest
+
 def verify_command(command):
     """
     Executes shell specified command and return the console output.
@@ -14,4 +20,16 @@ def verify_command(command):
         return subprocess.check_output(command, shell=True, universal_newlines=True, stderr=output).rstrip()
 
 if __name__ == '__main__':
-    print(verify_command('pmm-admin list'))
+    errors = []
+    listResponse = verify_command("pmm-admin list")
+    for line in listResponse.split("Port")[1].strip().splitlines():
+        if "pmm_agent" in line:
+            if "connected" not in line.lower():
+                errors.append(f"pmm_agent status should be 'Connected' but is: {line}")
+        else:
+            if "running" not in line.lower():
+                errors.append(f"pmm_agent status should be 'Running' but is: {line}")
+
+    if errors:
+        raise ValueError('\n'.join(errors))
+
